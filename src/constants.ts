7f1048a926ecfedc790af17561f43869ba169e8c
Rafael Cascalho <cascalhoeng@gmail.com>
2022-05-09 12:28:28 -0300

    fix: fixed secure key format constatnts

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
-export const KEY_START = '-----BEGIN PUBLIC KEY-----\n'
-export const KEY_END = '\n-----END PUBLIC KEY-----'
+export const KEY_START = '-----BEGIN PUBLIC KEY-----'
+export const KEY_END = '-----END PUBLIC KEY-----'
 export const DEFAULT_ALGORITHMS = ['RS256']
 export const ONE_MILLISECOND = 1000
